fc46a7739e08ac566ab8f9525734d185369a9031
joadio2 <jos674x@gmail.com>
2024-09-18 10:30:11 +0100

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://joadio2.github.io",
+  base: "astronew.io",
   integrations: [
     tailwind(),
     icon({
